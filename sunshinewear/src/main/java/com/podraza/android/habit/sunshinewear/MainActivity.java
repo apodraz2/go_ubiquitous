2a2b9e8bce4593e74ea4414e626f44ea888c101b
Adam Podraza <apodra86@gmail.com>
2016-01-17 11:36:20 -0700

    high, low, image all updating on wearable

--- sunshinewear/src/main/java/com/podraza/android/habit/sunshinewear/MainActivity.java
+++ sunshinewear/src/main/java/com/podraza/android/habit/sunshinewear/MainActivity.java
@@ -45,17 +45,29 @@ public class MainActivity extends WearableActivity {
 
         mHighView = (TextView) mContainerView.findViewById(R.id.high_view);
         mLowView = (TextView) mContainerView.findViewById(R.id.low_view);
+        mImageView = (ImageView) mContainerView.findViewById(R.id.weather_view);
+
+        if((Integer) mWeatherId == null) {
+            mHighView.setText(Double.toString(20));
+            mLowView.setText(Double.toString(10));
+
+            mImageView.setImageResource(getIconResourceForWeatherCondition(1));
+        }
 
 
         mMessageReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 Log.d(LOG_TAG, "onReceive");
-                mHigh = intent.getDoubleExtra("temp-high", 20);
-                mLow = intent.getDoubleExtra("temp-low", 0);
+                mHigh = Math.ceil(intent.getDoubleExtra("temp-high", 20));
+                mLow = Math.ceil(intent.getDoubleExtra("temp-low", 0));
                 mWeatherId = intent.getIntExtra("weather-id", 0);
 
 
+                mHighView.setText(Double.toString(mHigh));
+                mLowView.setText(Double.toString(mLow));
+
+                mImageView.setImageResource(getIconResourceForWeatherCondition(mWeatherId));
 
             }
         };
@@ -98,5 +110,34 @@ public class MainActivity extends WearableActivity {
             mHighView.setText((int) mHigh);
             mLowView.setText((int) mLow);
         }
+    }
+
+    public static int getIconResourceForWeatherCondition(int weatherId) {
+        // Based on weather code data found at:
+        // http://bugs.openweathermap.org/projects/api/wiki/Weather_Condition_Codes
+        if (weatherId >= 200 && weatherId <= 232) {
+            return R.drawable.ic_storm;
+        } else if (weatherId >= 300 && weatherId <= 321) {
+            return R.drawable.ic_light_rain;
+        } else if (weatherId >= 500 && weatherId <= 504) {
+            return R.drawable.ic_rain;
+        } else if (weatherId == 511) {
+            return R.drawable.ic_snow;
+        } else if (weatherId >= 520 && weatherId <= 531) {
+            return R.drawable.ic_rain;
+        } else if (weatherId >= 600 && weatherId <= 622) {
+            return R.drawable.ic_snow;
+        } else if (weatherId >= 701 && weatherId <= 761) {
+            return R.drawable.ic_fog;
+        } else if (weatherId == 761 || weatherId == 781) {
+            return R.drawable.ic_storm;
+        } else if (weatherId == 800) {
+            return R.drawable.ic_clear;
+        } else if (weatherId == 801) {
+            return R.drawable.ic_light_clouds;
+        } else if (weatherId >= 802 && weatherId <= 804) {
+            return R.drawable.ic_cloudy;
+        }
+        return -1;
     }
 }
